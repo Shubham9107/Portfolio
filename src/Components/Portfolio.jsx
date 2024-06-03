f27c8b26d795d0547c0c9ad554f228fc49b2cd98
Shubham9107 <joshishubham027@gmail.com>
2024-06-03 13:15:54 +0530

    first commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,86 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Amazon Clone",
+    description:
+      "This platform provides a hands-on demonstration of e-commerce best practices and user experience. It is scalable and adaptable.",
+    url: "https://amazon-clone-ruddy-five.vercel.app/",
+  },
+  {
+    title: "Restaurant Management System",
+    description:
+      "Dynamic Restaurant Web Application using React, offering an interactive menu, online reservations, and reviews integration.",
+    url: "https://restaurant-management-system-tb.netlify.app/",
+  },
+  {
+    title: "My Resume Site",
+    description:
+      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://shubham9107.github.io/Digital-Resume/",
+  },
+  {
+    title: "OS Algorithm Simulator",
+    description:
+      "OS Algorithm Simulator it Provides a Hands-on platform for understanding and experimenting with the core concepts of the Operating System.",
+    url: "os-projectteam8.netlify.app/",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
